6ed203ca5590b8ba415d1c1c96b35971e44a9682
Harshit-700 <harshitpal902@gmail.com>
2026-02-01 23:01:44 +0530

    Add trusted proxies for Railway load balancer

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (Railway load balancer)
+        $middleware->trustProxies(at: '*');
+        
         // Add CORS middleware globally
         $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
         
